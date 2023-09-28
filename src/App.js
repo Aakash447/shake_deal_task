6084b68a977638f0a6f44550579c1e9878994162
Aakash Sharma <s.aakash.lh@gmail.com>
2023-09-28 07:51:30 +0530

    created two input fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-group">
+        <label for="task">Enter task:</label>
+        <input type="textarea" id="task" className="input_textarea" />
+      </div>
+
+      <div className="input-group">
+        <label for="cars">Select a team:</label>
+
+        <select name="team" id="team">
+          <option value="">Select a team</option>
+        </select>
+      </div>
     </div>
   );
 }
